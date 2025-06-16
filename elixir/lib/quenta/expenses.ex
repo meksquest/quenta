ed618446b813d7e517c53a483343d196bb310da3
MMcClure11 <mmcclure0100@gmail.com>
2025-06-15 21:40:34 -0500

    feat: broadcast expense_added upon successful creation

--- elixir/lib/quenta/expenses.ex
+++ elixir/lib/quenta/expenses.ex
@@ -1,13 +1,21 @@
 defmodule Quenta.Expenses do
-  alias Quenta.Repo
   alias Quenta.Expenses.Expense
+  alias Quenta.PubSub
+  alias Quenta.Repo
 
   def change_expense(expense, attrs) do
     expense |> Expense.changeset(attrs)
   end
 
   def create_expense(attrs) do
-    %Expense{} |> Expense.changeset(attrs) |> Repo.insert()
+    case %Expense{} |> Expense.changeset(attrs) |> Repo.insert() do
+      {:ok, expense} ->
+        PubSub.broadcast_expense_added(expense)
+        {:ok, expense}
+
+      res ->
+        res
+    end
   end
 
   def list_expenses(opts \\ []) do
--- elixir/test/quenta/expenses_test.exs
+++ elixir/test/quenta/expenses_test.exs
@@ -2,86 +2,125 @@ defmodule Quenta.ExpensesTest do
   use Quenta.DataCase, async: true
   alias Quenta.Expenses
 
-  test "change_expense/2 returns a changeset for an existing expense" do
-    expense = %Quenta.Expenses.Expense{
-      id: 1,
-      description: "Test Expense",
-      amount_dollars: 10.00,
-      date: ~D[2023-10-01],
-      user_id: 1
-    }
+  describe "change_expense/2" do
+    test "returns a changeset for an existing expense" do
+      expense = %Quenta.Expenses.Expense{
+        id: 1,
+        description: "Test Expense",
+        amount_dollars: 10.00,
+        date: ~D[2023-10-01],
+        user_id: 1
+      }
 
-    changeset = Expenses.change_expense(expense, %{"description" => "Updated Expense"})
-    assert changeset.valid?
-    assert changeset.changes.description == "Updated Expense"
+      changeset = Expenses.change_expense(expense, %{"description" => "Updated Expense"})
+      assert changeset.valid?
+      assert changeset.changes.description == "Updated Expense"
+    end
+
+    test "returns an invalid changeset when missing required fields" do
+      expense = %Quenta.Expenses.Expense{
+        id: 1,
+        description: "Test Expense",
+        amount_dollars: 10.00,
+        date: ~D[2023-10-01],
+        user_id: 1
+      }
+
+      changeset = Expenses.change_expense(expense, %{"description" => nil})
+      refute changeset.valid?
+      assert [description: {"can't be blank", _}] = changeset.errors
+    end
   end
 
-  test "change_expense/2 return an invalid changeset when missing required fields" do
-    expense = %Quenta.Expenses.Expense{
-      id: 1,
-      description: "Test Expense",
-      amount_dollars: 10.00,
-      date: ~D[2023-10-01],
-      user_id: 1
-    }
+  describe "create_expense/1" do
+    test "creates a new expense" do
+      params = %{
+        "description" => "Lunch",
+        "amount_dollars" => 15.00,
+        "date" => ~D[2023-10-01],
+        "user_id" => 1
+      }
 
-    changeset = Expenses.change_expense(expense, %{"description" => nil})
-    refute changeset.valid?
-    assert [description: {"can't be blank", _}] = changeset.errors
-  end
+      assert {:ok, expense} = Expenses.create_expense(params)
+      assert expense.description == "Lunch"
+      assert expense.amount_cents == 1500
+      assert expense.date == ~D[2023-10-01]
+    end
 
-  test "create_expense/1 creates a new expense" do
-    params = %{
-      "description" => "Lunch",
-      "amount_dollars" => 15.00,
-      "date" => ~D[2023-10-01],
-      "user_id" => 1
-    }
+    test "creates a new expense and associated expense_item" do
+      params = %{
+        "description" => "Lunch",
+        "amount_dollars" => 15.00,
+        "date" => ~D[2023-10-01],
+        "user_id" => 1,
+        "expense_items" => [
+          %{
+            description: "Item 1",
+            amount_dollars: 5.00,
+            user_id: 2
+          }
+        ]
+      }
 
-    assert {:ok, expense} = Expenses.create_expense(params)
-    assert expense.description == "Lunch"
-    assert expense.amount_cents == 1500
-    assert expense.date == ~D[2023-10-01]
-  end
+      assert {:ok, expense} = Expenses.create_expense(params)
+      Quenta.Repo.preload(expense, :expense_items)
+      assert expense.description == "Lunch"
+      assert expense.amount_cents == 1500
+      assert expense.date == ~D[2023-10-01]
+      assert [expense_item] = expense.expense_items
+      assert expense_item.description == "Item 1"
+      assert expense_item.amount_cents == 500
+      assert expense_item.user_id == 2
+    end
 
-  test "create_expense/1 creates a new expense and associated expense_item" do
-    params = %{
-      "description" => "Lunch",
-      "amount_dollars" => 15.00,
-      "date" => ~D[2023-10-01],
-      "user_id" => 1,
-      "expense_items" => [
-        %{
-          description: "Item 1",
-          amount_dollars: 5.00,
-          user_id: 2
-        }
-      ]
-    }
+    test "returns an error when required fields are missing" do
+      params = %{
+        "description" => "Dinner",
+        # Missing amount_cents
+        "amount_cents" => nil,
+        "date" => ~D[2023-10-01],
+        "user_id" => 1
+      }
 
-    assert {:ok, expense} = Expenses.create_expense(params)
-    Quenta.Repo.preload(expense, :expense_items)
-    assert expense.description == "Lunch"
-    assert expense.amount_cents == 1500
-    assert expense.date == ~D[2023-10-01]
-    assert [expense_item] = expense.expense_items
-    assert expense_item.description == "Item 1"
-    assert expense_item.amount_cents == 500
-    assert expense_item.user_id == 2
-  end
+      assert {:error, changeset} = Expenses.create_expense(params)
+      assert changeset.valid? == false
+      assert changeset.errors[:amount_dollars] != nil
+    end
 
-  test "create_expense/1 returns an error when required fields are missing" do
-    params = %{
-      "description" => "Dinner",
-      # Missing amount_cents
-      "amount_cents" => nil,
-      "date" => ~D[2023-10-01],
-      "user_id" => 1
-    }
+    test "broadcasts expense_added upon success" do
+      params = %{
+        "description" => "Lunch",
+        "amount_dollars" => 15.00,
+        "date" => ~D[2023-10-01],
+        "user_id" => 1,
+        "expense_items" => [
+          %{
+            description: "Item 1",
+            amount_dollars: 5.00,
+            user_id: 2
+          }
+        ]
+      }
 
-    assert {:error, changeset} = Expenses.create_expense(params)
-    assert changeset.valid? == false
-    assert changeset.errors[:amount_dollars] != nil
+      Quenta.PubSub.subscribe_to_expense_added()
+      assert {:ok, %{id: created_expense_id}} = Expenses.create_expense(params)
+      assert_received {:expense_added, %{id: received_expense_id}}
+      assert created_expense_id == received_expense_id
+    end
+
+    test "does not broadcas expense_added upon failure" do
+      params = %{
+        "description" => "Dinner",
+        # Missing amount_cents
+        "amount_cents" => nil,
+        "date" => ~D[2023-10-01],
+        "user_id" => 1
+      }
+
+      Quenta.PubSub.subscribe_to_expense_added()
+      assert {:error, _changeset} = Expenses.create_expense(params)
+      refute_received {:expense_added, _}
+    end
   end
 
   test "list_expenses/0 returns an empty list when no expenses exist" do
